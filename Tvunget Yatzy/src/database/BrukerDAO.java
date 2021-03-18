7822cf7ad2e3f8020be6cead20a0f6e354028e10
Vebjoern Vaardal <Vebjoern Vaardal@DESKTOP-VI6RK0G>
2021-03-18 13:28:07 +0100

    Database

--- Tvunget Yatzy/src/database/BrukerDAO.java
+++ Tvunget Yatzy/src/database/BrukerDAO.java
@@ -1,8 +1,27 @@
 package database;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class BrukerDAO {
 
+	@PersistenceContext(name = "brukerPU")
+	private EntityManager em;
+	
+	public void lagreNyBruker(Bruker nyBruker) {
+		em.persist(nyBruker);
+	}
+	
+	public List<Bruker> hentAlleBrukere() {
+		return em.createQuery("SELECT b FROM bruker b ",Bruker.class).getResultList();
+	}
+	
+	public Bruker hentBruker(String brukernavn) {
+		return em.find(Bruker.class, brukernavn);
+	}
+	
 }
--- Tvunget Yatzy/src/database/ResultatDAO.java
+++ Tvunget Yatzy/src/database/ResultatDAO.java
@@ -1,8 +1,27 @@
 package database;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class ResultatDAO {
 
+	@PersistenceContext(name = "resultatPU")
+	private EntityManager em;
+	
+	public void lagreNyttResultat(Resultat nyttResultat) {
+		em.persist(nyttResultat);
+	}
+	
+	public List<Resultat> hentAlleResultat() {
+		return em.createQuery("SELECT r FROM resultat r",Resultat.class).getResultList();
+	}
+	
+	public Resultat hentResultat(Integer id) {
+		return em.find(Resultat.class, id);
+	}
+	
 }
