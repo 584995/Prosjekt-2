9236a4821b3e6f191c4a90922b6280d6bd4bb6e7
oyste <oyste@LAPTOP-6ETF7KCG.home>
2021-03-29 16:24:49 +0200

    useless tester

---
+++ Tvunget Yatzy/src/test/KastOgTerningTester.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import yatzy.Terning;
+import yatzy.Terningkast;
+
+public class KastOgTerningTester {
+
+	public Terning terning = new Terning();
+	public Terningkast terningKast1 = new Terningkast();
+	public Terningkast terningKast2 = new Terningkast();
+	
+	@Test
+	public void terningTest() {
+		
+	int øyne = terning.trill();
+	Boolean bool = false;
+	for(int i = 1; i < 6; i++) {
+		
+		if(øyne == i) {
+			bool = true;
+		}
+	}
+	assertTrue(bool);
+	
+	}
+	
+	@Test
+	public void TerningkastTest() {
+		
+		terningKast1.kast(3);
+		terningKast2.kast(3);
+		assertNotEquals(terningKast1, terningKast2);
+		
+	}
+}
